6eafd1a4af171e812ec60cd12bb553171d0cd713
Rob Brander <rob.brander@gmail.com>
2014-01-03 21:03:21 -0500

    v1.2

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@
 var Game = {
 	Answer: 0,
 	Guess: 0,
+	Round: 0,
 	Range: {
 		Min: 1,
 		Max: 5
@@ -10,9 +11,32 @@ var Game = {
 };
 
 Game.NewGame = function() {
-	Game.PickRandomMaxRange()
+	// Set defaults;
+	Game.Answer = 0;
+	Game.Guess = 0;
+	Game.Round = 0;
+	Game.Range.Min = 1;
+	Game.Range.Max = 5;
+	// Start the first round
+	Game.NewRound();
+};
+
+Game.NewRound = function() {
+	Game.Round++;
+	Game.HideMainMenu();
+	Game.ShowGame();
+	Game.PickRandomMaxRange();
 	Game.PickAnswer();
 	Game.CreateButtons();
+	Game.UpdateRoundText();
+};
+
+Game.HideMainMenu = function() {
+	$j('#divMainMenu').addClass('hidden');
+};
+
+Game.ShowGame = function() {
+	$j('#divGameContents').removeClass('hidden');
 };
 
 Game.PickRandomMaxRange = function() {
@@ -22,9 +46,21 @@ Game.PickRandomMaxRange = function() {
 
 Game.CreateButtons = function() {
 	var div = $j('div#buttonSelectors');
-	var html = '<button class="btnGuess" type="button" onclick="Game.GuessAnswer(1)">1</button>';
+	var html = '<button id="btnGuess1" class="btnGuess" type="button" onclick="Game.GuessAnswer(1)">1</button>';
+	div.addClass('hidden');
+	var divHTML = '';
 	for (var num = Game.Range.Min; num <= Game.Range.Max; num++)
-		div.append(html.replace(/1/g, num.toString()));
+		divHTML += html.replace(/1/g, num.toString());
+	div.html(divHTML);
+	div.removeClass('hidden');
+};
+
+Game.UpdateRoundText = function() {
+	Game.divGameMsg = $j('#divGameMessage');
+	$j('#spanRoundNum').text(Game.Round.toString());
+	$j('#spanMinRange').text(Game.Range.Min.toString());
+	$j('#spanMaxRange').text(Game.Range.Max.toString());
+	$j('#divGameMessage').text('').addClass('hidden');
 };
 	
 Game.PickAnswer = function() {
@@ -35,28 +71,52 @@ Game.PickAnswer = function() {
 
 Game.GuessAnswer = function(guess) {
 	Game.Guess = guess;
+	
+	// hide button
+	var btn = $j('#btnGuess'+guess.toString());
+	btn.attr('onclick', '');
+	btn.fadeOut(900, function() {
+		btn
+			.css ('display','')
+			.css ('visibility','hidden');
+	});
+	
 	if (Game.Guess === Game.Answer)
 		Game.FoundAnswer();
 	else
 		Game.IncorrectGuess();
 };
 
+
+Game.ShowMessage = function(msg, style) {
+	var fadeDuration = 900;	// milliseconds
+	Game.divGameMsg
+		.attr('class', '')
+		.addClass('hidden')
+		.addClass(style)
+		.text(msg)
+		.fadeIn(fadeDuration, function() {
+			setTimeout(function() {
+				Game.divGameMsg.fadeOut(fadeDuration, function() {
+					Game.divGameMsg.addClass('hidden');
+				});
+			}, fadeDuration);
+		});
+};
+
 Game.FoundAnswer = function() {
-	alert('you found the answer!');
+	Game.ShowMessage('You found the answer!', 'good');
 	if (confirm('want to play again?'))
-		Game.NewGame();
+		Game.NewRound();
 	else
 		Game.Over();
 };
 
 Game.IncorrectGuess = function() {
-	alert('incorrect guess: ' + Game.Guess);
+	Game.ShowMessage('Incorrect, try again', 'bad');
 };
 
 Game.Over = function() {
-	$j('#divGameContents').hide();
+	$j('#divGameContents').addClass('hidden');
+	$j('#divMainMenu').removeClass('hidden');
 };
-
-$j(function() {
-	Game.NewGame();
-});
