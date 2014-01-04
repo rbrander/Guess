0140a09f6f7a2389b362a85341c1cac1ff1694cd
Rob Brander <rob.brander@gmail.com>
2014-01-03 20:06:29 -0500

    v1.1

--- javascript.js
+++ javascript.js
@@ -10,14 +10,27 @@ var Game = {
 };
 
 Game.NewGame = function() {
+	Game.PickRandomMaxRange()
 	Game.PickAnswer();
+	Game.CreateButtons();
+};
+
+Game.PickRandomMaxRange = function() {
+	// Pick an upper range between 5 and 10
+	Game.Range.Max = 5 + Math.floor(Math.random() * 6);	// random() returns between 0 and 0.99 (never 1)
+};
+
+Game.CreateButtons = function() {
+	var div = $j('div#buttonSelectors');
+	var html = '<button class="btnGuess" type="button" onclick="Game.GuessAnswer(1)">1</button>';
+	for (var num = Game.Range.Min; num <= Game.Range.Max; num++)
+		div.append(html.replace(/1/g, num.toString()));
 };
 	
 Game.PickAnswer = function() {
 	// Math.random() provides a number between 0 and up to but not including 1
 	var diff = (Game.Range.Max + 1) - Game.Range.Min;
 	Game.Answer = Math.floor(Game.Range.Min + (Math.random() * diff));
-console.log("Game.Answer = " + Game.Answer);
 };
 
 Game.GuessAnswer = function(guess) {
